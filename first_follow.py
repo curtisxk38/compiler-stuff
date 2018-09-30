a5297daf2c1f92d82a2ab8a247a07e4427a01db4
curtisxk38 <ck2ha@virginia.edu>
2018-09-30 18:44:24 -0400

    cover edge cases in first/follow

--- first_follow.py
+++ first_follow.py
@@ -9,14 +9,17 @@ def base_follow(follow, first, g):
         if len(rule["R"]) > 1:
             last_sym = rule["R"][-1]
             second_last_sym = rule["R"][-2]
-            # copy first[last_sym]
-            first_last_sym = first[last_sym].copy()
-            # remove "" from it
-            try:
-                first_last_sym.remove("")
-            except KeyError:
-                pass
-            follow[second_last_sym].update(first_last_sym)
+            # only proceed if second_last_sym is a non terminal
+            #  if its terminal, we don't need a follow() set for it
+            if second_last_sym in follow:
+                # copy first[last_sym]
+                first_last_sym = first[last_sym].copy()
+                # remove "" from it
+                try:
+                    first_last_sym.remove("")
+                except KeyError:
+                    pass
+                follow[second_last_sym].update(first_last_sym)
 
 
 def get_follow(g, first):
@@ -27,14 +30,35 @@ def get_follow(g, first):
     
     while True:
         change_made = False
-        for rule in g.grammar["rules"]:
+        print(follow)
+        for rule in g.grammar["rules"]:             
             # if B -> YXA, and "" in First(A), then Follow(B) is in Follow(A)
             if len(rule["R"]) > 1 and "" in first[rule["R"][-1]]:
                 second_last_sym = rule["R"][-2]
-                # keep track of size of follow[second_last_sym] to see if changes are made
-                old_size = len(follow[second_last_sym])
-                follow[second_last_sym].update(follow[rule["L"]])
-                if old_size != len(follow[second_last_sym]):
+                # only proceed if second_last_sym is a non terminal
+                #  if its terminal, we don't need a follow() set for it
+                if second_last_sym in follow:
+                    # keep track of size of follow[second_last_sym] to see if changes are made
+                    old_size = len(follow[second_last_sym])
+                    # update follow
+                    follow[second_last_sym].update(follow[rule["L"]])
+                    # check if size is different, that means somethign was added to the follow set, 
+                    #  and changes were made
+                    if old_size != len(follow[second_last_sym]):
+                        change_made = True
+            
+            # logically the same 'recursive' case as above, but have to handle this:
+            #  if B -> X, then follow(B) in follow(A), because A becomes the empty string, so its like "" is in it
+            #   we just don't represent RHS in our grammar as having [... "X", ""] that extra "" at the end
+            # only relevant if X is a nonterminal
+            elif len(rule["R"]) == 1 and rule["R"][0] in follow:
+                # keep track of size of follow set to see if changes are made
+                old_size = len(follow[rule["R"][0]])
+                # update follow
+                follow[rule["R"][0]].update(follow[rule["L"]])
+                # check if size is different, that means somethign was added to the follow set, 
+                #  and changes were made
+                if old_size != len(follow[rule["R"][0]]):
                     change_made = True
 
         if not change_made:
@@ -70,6 +94,18 @@ def get_first(g):
                     first[rule["L"]].update(first[sym])
                     all_empty = False
                     break
+            # also have add first(A) where A is the first symbol of RHS, regardless of whether "" in first(A)
+            #  this is intuitive, because you could imagine RHS being ["", "A", ...], but the for loop won't handle
+            #   this case because we didn't explicitly put the "" at the beginning of the RHS
+            to_add = first[rule["R"][0]].copy()
+            # don't want to add the empty string "", if its in to_add because we should only
+            #  add the empty string for in the special case where everything in RHS goes to "" (covered below)
+            try:
+                to_add.remove("")
+            except KeyError:
+                pass
+            first[rule["L"]].update(to_add)
+
             # if all symbols in RHS of this rule go to ""
             #  then "" is in the first of LHS of this rule
             if all_empty:
