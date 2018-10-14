3964a937689d19a47e2f537325c07392ae82ecb6
curtisxk38 <ck2ha@virginia.edu>
2018-10-14 12:29:35 -0400

    Add class for rules in CFG

--- first_follow.py
+++ first_follow.py
@@ -2,13 +2,13 @@ import grammar
 
 def base_follow(follow, first, g):
     # $ is in Follow(start symbol)
-    follow[g.grammar["start"]].add("$")
+    follow[g.start].add("$")
     
-    for rule in g.grammar["rules"]:
+    for rule in g.rules:
         # if B -> YXA, First(A) - "" is in Follow(X)
-        if len(rule["R"]) > 1:
-            last_sym = rule["R"][-1]
-            second_last_sym = rule["R"][-2]
+        if len(rule.rhs) > 1:
+            last_sym = rule.rhs[-1]
+            second_last_sym = rule.rhs[-2]
             # only proceed if second_last_sym is a non terminal
             #  if its terminal, we don't need a follow() set for it
             if second_last_sym in follow:
@@ -23,25 +23,24 @@ def base_follow(follow, first, g):
 
 
 def get_follow(g, first):
-    follow = {k: set() for k in g.grammar["nonterm"]}
+    follow = {k: set() for k in g.nonterm}
     # cover base cases first
     base_follow(follow, first, g)
 
     
     while True:
         change_made = False
-        print(follow)
-        for rule in g.grammar["rules"]:             
+        for rule in g.rules:             
             # if B -> YXA, and "" in First(A), then Follow(B) is in Follow(A)
-            if len(rule["R"]) > 1 and "" in first[rule["R"][-1]]:
-                second_last_sym = rule["R"][-2]
+            if len(rule.rhs) > 1 and "" in first[rule.rhs[-1]]:
+                second_last_sym = rule.rhs[-2]
                 # only proceed if second_last_sym is a non terminal
                 #  if its terminal, we don't need a follow() set for it
                 if second_last_sym in follow:
                     # keep track of size of follow[second_last_sym] to see if changes are made
                     old_size = len(follow[second_last_sym])
                     # update follow
-                    follow[second_last_sym].update(follow[rule["L"]])
+                    follow[second_last_sym].update(follow[rule.lhs])
                     # check if size is different, that means somethign was added to the follow set, 
                     #  and changes were made
                     if old_size != len(follow[second_last_sym]):
@@ -51,14 +50,14 @@ def get_follow(g, first):
             #  if B -> X, then follow(B) in follow(A), because A becomes the empty string, so its like "" is in it
             #   we just don't represent RHS in our grammar as having [... "X", ""] that extra "" at the end
             # only relevant if X is a nonterminal
-            elif len(rule["R"]) == 1 and rule["R"][0] in follow:
+            elif len(rule.rhs) == 1 and rule.rhs[0] in follow:
                 # keep track of size of follow set to see if changes are made
-                old_size = len(follow[rule["R"][0]])
+                old_size = len(follow[rule.rhs[0]])
                 # update follow
-                follow[rule["R"][0]].update(follow[rule["L"]])
+                follow[rule.rhs[0]].update(follow[rule.lhs])
                 # check if size is different, that means somethign was added to the follow set, 
                 #  and changes were made
-                if old_size != len(follow[rule["R"][0]]):
+                if old_size != len(follow[rule.rhs[0]]):
                     change_made = True
 
         if not change_made:
@@ -69,10 +68,10 @@ def get_follow(g, first):
 def base_first(first, g):
     for sym in first:
         # sym is a terminal, first(sym) = [sym]
-        if sym in g.grammar["term"]:
+        if sym in g.term:
             first[sym].add(sym)
         # sym is non terminal and sym -> "", so first[sym] has ""
-        elif {"L":sym,"R":[""]} in g.grammar["rules"]:
+        elif {"L":sym,"R":[""]} in g.rules:
             first[sym].add("")
 
 def get_first(g):
@@ -83,35 +82,35 @@ def get_first(g):
     while True:
         change_made = False
         # iterate over rules in grammar
-        for rule in g.grammar["rules"]:
-            # keep track of size of first[rule["L"]] to see if changes are made
-            old_size = len(first[rule["L"]])
+        for rule in g.rules:
+            # keep track of size of first[rule.lhs] to see if changes are made
+            old_size = len(first[rule.lhs])
             # find the first symbol in RHS of rule,
             #  where the symbol does not go to ""
             all_empty = True
-            for sym in rule["R"]:
+            for sym in rule.rhs:
                 if "" not in first[sym]:
-                    first[rule["L"]].update(first[sym])
+                    first[rule.lhs].update(first[sym])
                     all_empty = False
                     break
             # also have add first(A) where A is the first symbol of RHS, regardless of whether "" in first(A)
             #  this is intuitive, because you could imagine RHS being ["", "A", ...], but the for loop won't handle
             #   this case because we didn't explicitly put the "" at the beginning of the RHS
-            to_add = first[rule["R"][0]].copy()
+            to_add = first[rule.rhs[0]].copy()
             # don't want to add the empty string "", if its in to_add because we should only
             #  add the empty string for in the special case where everything in RHS goes to "" (covered below)
             try:
                 to_add.remove("")
             except KeyError:
                 pass
-            first[rule["L"]].update(to_add)
+            first[rule.lhs].update(to_add)
 
             # if all symbols in RHS of this rule go to ""
             #  then "" is in the first of LHS of this rule
             if all_empty:
-                first[rule["L"]].add("")
+                first[rule.lhs].add("")
 
-            if old_size != len(first[rule["L"]]):
+            if old_size != len(first[rule.lhs]):
                 change_made = True
         
         # do the 'recursive' step until no changes are made to any first sets        
--- grammar.py
+++ grammar.py
@@ -5,5 +5,21 @@ class Grammar:
         with open(json_file, "r") as f:
                 self.grammar = json.load(f)
 
+        self.rules = []
+        for rule in self.grammar["rules"]:
+        	self.rules.append(Rule(rule))
+
+        self.start = self.grammar["start"]
+        self.nonterm = self.grammar["nonterm"]
+        self.term = self.grammar["term"]
+
     def all_symbols(self):
-        return self.grammar["nonterm"] + self.grammar["term"]
+        return self.nonterm + self.term
+
+class Rule:
+	def __init__(self, json_rule):
+		self.lhs = json_rule["L"]
+		self.rhs = json_rule["R"]
+
+	def __repr__(self):
+		return "{} -> {}".format(self.left, self.right)
